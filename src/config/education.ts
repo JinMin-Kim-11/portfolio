959d7978d90bde5b022439b8f14e59028cfa630a
JinMin <pollardpollard5h71vjh@gmail.com>
2026-08-13 16:08:12 +0800

    Remove 本科/专科 labels from education section

--- src/config/education.ts
+++ src/config/education.ts
@@ -14,14 +14,14 @@ export type EducationItemType = {
   export const educationList: Array<EducationItemType> = [
     {
       school: '上海建桥学院',
-      major: '软件工程（本科）',
+      major: '软件工程',
       logo: 'college',
       start: '2024',
       end: '2026'
     },
     {
       school: '上海建桥学院',
-      major: '计算机科学技术（专科）',
+      major: '计算机科学技术',
       logo: 'college',
       start: '2021',
       end: '2024'
