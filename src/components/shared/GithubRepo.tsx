245e7ff4bcc5534684b3e565a828d70b77debfac
JinMin-Kim-11 <pollardpollard5h71vjh@gmail.com>
2026-08-13 15:55:13 +0800

    Fix GitHub link in GithubRepo.tsx

--- src/components/shared/GithubRepo.tsx
+++ src/components/shared/GithubRepo.tsx
@@ -7,7 +7,7 @@ import Link from "next/link"
 export function GithubRepo() {
   return (
     <Link
-      href="https://github.com/jinmin"
+      href="https://github.com/JinMin-Kim-11"
       target="_blank"
       rel="noopener noreferrer nofollow"
       className="group flex flex-row  justify-start items-center text-md font-medium transition hover:text-teal-500 dark:text-zinc-200 dark:hover:text-teal-500"
