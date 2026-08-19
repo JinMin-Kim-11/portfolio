82d780c4fe092751325559e8f92cc329ac479196
JinMin-Kim-11 <pollardpollard5h71vjh@gmail.com>
2026-08-19 13:46:05 +0800

    add version-filtered blog system with 3 new posts

---
+++ src/app/pm/blogs/[slug]/page.tsx
@@ -0,0 +1,44 @@
+import { type Metadata } from 'next'
+import { notFound } from 'next/navigation'
+import { getBlogsByVersion, getBlogBySlug } from '@/lib/blogs'
+import { getMDXContent } from '@/lib/mdx'
+import { BlogLayout } from '@/components/layout/BlogLayout'
+
+interface Props {
+  params: Promise<{ slug: string }>
+}
+
+export async function generateStaticParams() {
+  const blogs = await getBlogsByVersion('pm')
+  return blogs.map((blog) => ({
+    slug: blog.slug,
+  }))
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { slug } = await params
+  const blog = await getBlogBySlug(slug)
+  if (!blog) {
+    return { title: '博客未找到' }
+  }
+  return {
+    title: blog.title,
+    description: blog.description,
+  }
+}
+
+export default async function PMBlogPage({ params }: Props) {
+  const { slug } = await params
+  const blog = await getBlogBySlug(slug)
+  if (!blog) {
+    notFound()
+  }
+  const content = await getMDXContent(slug)
+  return (
+    <BlogLayout blog={blog}>
+      <div className="mt-8 prose dark:prose-invert max-w-none">
+        {content}
+      </div>
+    </BlogLayout>
+  )
+}
